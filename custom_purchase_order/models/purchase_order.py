ca93ad4ffcfe59b77c5653a100ca37f34af481bf
Christopher García <gutierrez.cg33@gmail.com>
2022-05-19 11:23:34 -0400

    [ADD] configuración de limitación de lineas

--- custom_purchase_order/models/purchase_order.py
+++ custom_purchase_order/models/purchase_order.py
@@ -2,10 +2,23 @@
 from odoo import api, fields, models
 
 
+class Company(models.Model):
+    _inherit = 'res.company'
+    
+    qty_lines_on_purchase_line = fields.Integer(string="lineas de consulta de productos por pedido", default=5)
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    qty_lines_on_purchase_line = fields.Integer(string="lineas de consulta de productos por pedido", related="company_id.qty_lines_on_purchase_line",readonly=False)
+
+
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     purchase_query_ids = fields.One2many('purchase.order', 'order_line_query_id', string="Compras del producto",compute="_compute_result_purchase")
+
 
     @api.depends("order_id.order_line")
     def _compute_result_purchase(self):
@@ -17,7 +30,7 @@ class PurchaseOrderLine(models.Model):
                 for order_line in pur.order_line:
                     if rec.product_id.id == order_line.product_id.id:
                         id_purchaces.append(pur.id)
-            for line in self.env['purchase.order'].browse(set(id_purchaces)):
+            for line in self.env['purchase.order'].search([('id','in',list(set(id_purchaces)))],order="create_date desc",limit=rec.company_id.qty_lines_on_purchase_line):
                 rec.write({'purchase_query_ids': [(4, line.id)]})
                 rec.purchase_query_ids.write({'product_query': rec.product_id.id})
             
@@ -50,7 +63,7 @@ class PurchaseOrder(models.Model):
     product_qty = fields.Float(string="Cantidad Pedida",compute="_compute_product_qty") 
     qty_received = fields.Float(string="Cantidad Recibida",compute="_compute_qty_received") 
     price_unit = fields.Float(string="Costo Neto",compute="_compute_price_unit") 
-    bodeg = fields.Char(string="Sucursal") 
+    bodeg = fields.Char(string="Sucursal",compute="_compute_bodeg") 
     
 
     def _compute_product_qty(self):
@@ -90,3 +103,9 @@ class PurchaseOrder(models.Model):
             rec.date_invoice = False
             if rec.invoice_ids:
                 rec.date_invoice = rec.invoice_ids[0].date
+
+    def _compute_bodeg(self):
+        for rec in self:
+            rec.bodeg = False
+            if rec.picking_ids:
+                rec.bodeg = str(rec.picking_ids[0].location_dest_id.name)
